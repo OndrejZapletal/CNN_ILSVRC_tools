596bbd4ee93c9110f4b074aada9834c6d6d5e751
Ondrej Zapletal <ondrej.matej.oz@gmail.com>
2017-04-21 06:18:57 +0200

    first working version

--- keras_module.py
+++ keras_module.py
@@ -1,57 +1,76 @@
+import logging
 import sys
 
 import numpy as np
-from keras.datasets import cifar10
-from keras.layers import (Activation, Conv2D, Dense, Dropout, Flatten,
-                          MaxPooling2D)
-from keras.models import Sequential
-from keras.utils import np_utils
+
+
+# from keras.datasets import cifar10
+# from keras.layers import (Activation, Conv2D, Dense, Dropout, Flatten,
+#                           MaxPooling2D)
+# from keras.models import Sequential
+# from keras.utils import np_utils
+
+
+def create_logger():
+    # creation of logger
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.DEBUG)
+
+    # logging format
+    formatter = logging.Formatter('%(asctime)s - %(lineno)d - %(name)s - %(levelname)s - %(message)s')
+
+    # file handler
+    handler_dbg = logging.FileHandler('keras_debug.log')
+    handler_dbg.setLevel(logging.DEBUG)
+    handler_dbg.setFormatter(formatter)
+
+    # stream handler
+    handler_inf = logging.StreamHandler()
+    handler_inf.setLevel(logging.INFO)
+    handler_inf.setFormatter(formatter)
+
+    # add the handlers to the logger
+    logger.addHandler(handler_dbg)
+    logger.addHandler(handler_inf)
+    return logger
+
+
+LOGGER_KERAS = create_logger()
 
 
 def test_model(model, optimizers, dataset):
     """main function"""
+    LOGGER_KERAS.info("testing model: %s", str(model))
+    find_performance_of_model(model, optimizers, dataset)
+    return True
 
-    loaded_model = load_model_from_file(model)
+def find_performance_of_model(model_data, optimizers, data):
+    """ Function will test results of different optimizers. """
+    for optimizer in optimizers:
+        LOGGER_KERAS.info('Testing model %s with optimizer %s', str(model_data[1]['name']), optimizer)
+        trained_model = train_model(model_data, data[0], optimizer)
+        # save_model_to_file(trained_model, '%s_%s' % (i, optimizer))
+        evaluate_net(trained_model, data[1])
 
-    find_performance_of_model(loaded_model, optimizers, dataset)
 
-def find_performance_of_model(models, optimizers, data):
-    """ Function will test results of different optimizers.
-    """
-    for i, model in enumerate(models, start=4):
-        for optimizer in optimizers:
-            print('\nTesting model %s with optimizer %s' % (i, optimizer))
-            trained_model = train_model(model(), data[0], optimizer)
-            save_model_to_file(trained_model, '%s_%s' % (i, optimizer))
-            evaluate_net(trained_model, data[1])
+# def save_model_to_file(model, name):
+#     """Save model to json and weigths to h5py."""
+#     try:
+#         with open("../trained_models/model_%s_architecture.json" % name, 'w') as json_file:
+#             json_file.write(model.to_json())
+#         # model.save_weights("model_%s_weights.h5" % name)
+#         # print("model_%s saved" % name)
+#         return True
+#     except:
+#         print(sys.exc_info())
+#         return False
 
-def load_model_from_file(model):
-    """Save model to json and weigths to h5py."""
-    try:
-        with open("%s.json" % model[0], 'r') as json_file:
-            json_file.write(model.to_json())
-        return True
-    except:
-        print(sys.exc_info())
-        return False
-
-def save_model_to_file(model, name):
-    """Save model to json and weigths to h5py."""
-    try:
-        with open("model_%s_architecture.json" % name, 'w') as json_file:
-            json_file.write(model.to_json())
-        model.save_weights("model_%s_weights.h5" % name)
-        print("model_%s saved" % name)
-        return True
-    except:
-        print(sys.exc_info())
-        return False
 
 def evaluate_net(model, test_data):
     """Evaluates preformamce of the network on the test data."""
     score = model.evaluate(test_data[0], test_data[1], verbose=0)
-    print('Test score:', score[0])
-    print('Test accuracy:', score[1])
+    LOGGER_KERAS.info('Test score: %s', score[0])
+    LOGGER_KERAS.info('Test accuracy: %s', score[1])
 
     predicted_classes = model.predict(test_data[0])
     predicted_classes_indexes = [np.argmax(item) for item in predicted_classes]
@@ -66,22 +85,24 @@ def evaluate_net(model, test_data):
         if test_data_indexes[i] != predicted_classes_indexes[i]
     ]
 
-    print("Correctly guessed: %s" % len(correct_indexes))
-    print("Incorrectly guessed: %s" % len(incorrect_indexes))
+    LOGGER_KERAS.info("Correctly guessed: %s", len(correct_indexes))
+    LOGGER_KERAS.info("Incorrectly guessed: %s", len(incorrect_indexes))
 
     return score
 
-def train_model(model, train_data, optimizer):
+def train_model(model_data, train_data, optimizer):
     """Trains Neural Network with available data."""
+    model = model_data[0]
+    model_configuration = model_data[1]
     model.compile(
-        loss='categorical_crossentropy',
+        loss=model_configuration['loss'],
         optimizer=optimizer,
-        metrics=['accuracy'])
+        metrics=[model_configuration['metrics']])
     model.fit(
         train_data[0],
         train_data[1],
-        batch_size=128,
-        epochs=1,
-        verbose=1,
-        validation_split=0.1)
+        batch_size=model_configuration['batch_size'],
+        epochs=model_configuration['epochs'],
+        verbose=model_configuration['verbose'],
+        validation_split=model_configuration['validation_split'])
     return model
--- test_runner.py
+++ test_runner.py
@@ -12,47 +12,118 @@
 # - run model and save results and weights
 
 import glob
+import json
+import logging
+import os
+import re
 import time
 
+from keras.datasets import cifar10
+from keras.models import model_from_json
+from keras.utils import np_utils
 
-# from keras_module import test_model
+from keras_module import test_model
 
+TEMP_FILE = '#in_progress#'
 
-def load_model(source_name):
+def create_logger():
+    """Function that creates logger for this module. """
+    # creation of logger
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.DEBUG)
+
+    # logging format
+    formatter = logging.Formatter('%(asctime)s - %(lineno)d - %(name)s - %(levelname)s - %(message)s')
+
+    # file handler
+    handler_dbg = logging.FileHandler('application_debug.log')
+    handler_dbg.setLevel(logging.DEBUG)
+    handler_dbg.setFormatter(formatter)
+
+    # stream handler
+    handler_inf = logging.StreamHandler()
+    handler_inf.setLevel(logging.INFO)
+    handler_inf.setFormatter(formatter)
+
+    # add the handlers to the logger
+    logger.addHandler(handler_dbg)
+    logger.addHandler(handler_inf)
+    return logger
+
+LOGGER_APP = create_logger()
+
+def load_model_data(source_name):
     """TODO: Make sure that you are getting correct information from both configuration and parameter files."""
-
-    model = get_model_parameters(source_name), get_model_configuration(source_name)
-    return model
+    model = get_model_from_json(source_name)
+    configuration = get_model_configuration_from_json(source_name)
+    return (model, configuration)
 
 
 def check_for_new_models(list_of_models):
     """TODO: retrun value of each model_name should contain only unique part of the name.
     i.e timestamp and itteration.
     """
-    list_of_files = glob.glob('../models/model*.txt')
+    list_of_files = glob.glob('../models/model_*_parameters.json')
     new_models = []
-    for model_name in list_of_files:
+    for file_name in list_of_files:
+        # TODO: Don't forget to handle the exception
+        model_name = parse_model_name(file_name)
         if model_name not in list_of_models:
             new_models.append(model_name)
     return new_models
 
+def parse_model_name(file_name):
+    """Parser is extracting part of the name betwen words 'model_' and '_parameters.json'."""
+    file_parser = re.compile(r"\.\./models/model_(.*)_parameters\.json")
+    result = file_parser.match(file_name).group(1)
+    LOGGER_APP.debug(result)
+    if not result:
+        LOGGER_APP.error("Name of model wasn't parsed.")
 
-def get_model_configuration(source):
-    """TODO: This function will read vales from JSON to configure model.
-    """
-    return source
+        Exception("Name of model wasn't parsed.")
+    # TODO
+    return result
 
 
-def get_model_parameters(source):
+def get_model_from_json(source):
     """TODO: This function will read vales from text file to set model paramters."""
-    return source
+    with open("../models/model_%s_parameters.json" % source, 'r') as json_file:
+        loaded_model_json = json_file.read()
+        return model_from_json(loaded_model_json)
+
+
+def get_model_configuration_from_json(source):
+    """TODO: This function will read vales from JSON to configure model. """
+    with open("../models/model_%s_configuration.json" % source, 'r') as json_file:
+        return json.loads(json_file.read())
+
 
 
 def load_dataset():
     """TODO:
-    Think how to make this as independet from the rest of the script as possible."""
+    Think how to make this as independet from the rest of the script as possible.
+    Load data from CIFAR10 database."""
+    nb_classes = 10
+    max_val = 255
+    (x_train, y_train), (x_test, y_test) = cifar10.load_data()
+    x_train = x_train.astype('float32')
+    x_test = x_test.astype('float32')
+    x_train /= max_val
+    x_test /= max_val
+    LOGGER_APP.debug("Training matrix shape %s", x_train.shape)
+    LOGGER_APP.debug("Testing matrix shape %s", x_test.shape)
+    y_train = np_utils.to_categorical(y_train, nb_classes)
+    y_test = np_utils.to_categorical(y_test, nb_classes)
+    return ((x_train, y_train), (x_test, y_test))
 
-    return 1
+
+def load_optimizers():
+    """Function loades optimizers from standalone file."""
+    optimizers = ['adam', 'nadam']
+    with open("../configuration/optimizers.txt", 'r') as optimizers_file:
+        optimizers = optimizers_file.read().strip().split(',')
+        LOGGER_APP.debug('loading optimizers from file: %s', optimizers)
+    return optimizers
 
 
 def main_loop():
@@ -61,32 +132,50 @@ def main_loop():
     """
     trained_models = []
     dataset = load_dataset()
-    optimizers = ['adam', 'nadam']
     while True:
+        optimizers = load_optimizers()
         new_models = check_for_new_models(trained_models)
         if new_models:
             for model_name in new_models:
-                model_descrition = load_model(model_name)
                 model_sucessfully_tested = False
+
+                LOGGER_APP.info("Found model: %s", str(model_name))
                 try:
-                    model_sucessfully_tested = test_model(model_descrition, optimizers, dataset)
-                except:
-                    # General exception, TODO: find better exception
-                    print("Model %s"% model_descrition)
+
+                    model_data = load_model_data(model_name)
+
+                    try:
+                        with open(TEMP_FILE, 'w') as temp_file:
+                            temp_file.write("%s\n" % time.time())
+                            temp_file.write("%s\n" % str(model_data[1]['name']))
+                            temp_file.write("%s" % str(optimizers))
+
+                        model_sucessfully_tested = test_model(model_data, optimizers, dataset)
+
+                        try:
+                            os.remove(TEMP_FILE)
+                        except OSError:
+                            LOGGER_APP.debug("This might be a problem since this shuldn't happen!")
+                    except:
+                        # General exception, TODO: find better exception
+                        LOGGER_APP.info("Model: %s", str(model_data[1]['name']))
+                        try:
+                            os.remove(TEMP_FILE)
+                        except OSError:
+                            LOGGER_APP.debug("Temp file not present while handeling exception.")
+                except FileNotFoundError:
+                    LOGGER_APP.info("Configuration file is missing!")
 
                 if model_sucessfully_tested:
                     trained_models.append(model_name)
-                    print("Found model: %s" % model_name)
+                    LOGGER_APP.info("Succesfully tested model: %s", str(model_name))
         wait_time_interval(10)
 
 
-def test_model(model_descrition, optimizers, dataset):
-    """TEST FUNCTION"""
-    return True
-
 
 def wait_time_interval(interval):
-    time.sleep(interval)
+    """Function defines time interval that will be waited between individual test."""
+    time.sleep(interval*60)
 
 
 if __name__ == '__main__':
